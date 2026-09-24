d6933050775f27cc18db563078ac6455a6f7927b
André Chagas <andreluis_chagas@hotmail.com>
2026-09-24 20:45:10 -0300

    fix: Trabalhando com classe de modelo e Arrays

--- Conceitos_basicos/src/app/lista-compras/lista-compras.component.ts
+++ Conceitos_basicos/src/app/lista-compras/lista-compras.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms'
+import { ItemLista } from './itemlista'
 
 @Component({
   selector: 'app-lista-compras',
@@ -10,9 +11,17 @@ import { FormsModule } from '@angular/forms'
 export class ListaComprasComponent {
 
   item: string = '';
+  lista: ItemLista[] = [];
 
   adicionarItem() {
-    console.log("Item recebido: ", this.item);
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+
+    this.lista.push(itemLista);
+
     this.item = '';
+
+    console.table(this.lista)
   }
 }
